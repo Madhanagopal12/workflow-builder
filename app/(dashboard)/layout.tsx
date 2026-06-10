0db4ed7ec6db3a4076aead4f81a83633b1657fdb
Madhanagopal12 <Madhanagopal2004@gmail.com>
2026-06-11 01:04:27 +0530

    Initial commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Separator } from "@/components/ui/separator";
+import DesktopSidebar from "@/components/DesktopSidebar";
+import BreadcrumbHeader from "@/components/BreadcrumbHeader";
+import { ModeToggle } from "@/components/ThemeModeToggle";
+import { UserButton } from "@clerk/nextjs";
+
+function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen">
+      <DesktopSidebar />
+      <div className="flex flex-col flex-1 min-h-screen">
+        <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
+          <BreadcrumbHeader />
+          <div className="flex items-center gap-1">
+            <ModeToggle />
+            <UserButton />
+          </div>
+        </header>
+        <Separator />
+        <div className="overflow-auto">
+          <div className="flex flex-1 container py-4 px-2 text-accent-foreground">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default layout;
---
+++ components/providers/AppProvider.tsx
@@ -0,0 +1,10 @@
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+export function AppProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+}
