ec90daca39a9db700146b0c12e09ed6064b3f9d8
Madhanagopal12 <Madhanagopal2004@gmail.com>
2026-06-16 00:14:01 +0530

    fix: setup Prisma client with Better SQLite adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,10 @@
-// lib/prisma.ts
-import { PrismaClient } from "./generated/prisma/client";
+import "dotenv/config";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "@/lib/generated/prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const connectionString = `${process.env.DATABASE_URL}`;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const prisma = new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export { prisma };
