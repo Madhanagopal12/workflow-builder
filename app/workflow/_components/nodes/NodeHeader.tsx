d19a4a4971e836cc43805875ef287bb667b8bca7
Madhanagopal12 <Madhanagopal2004@gmail.com>
2026-07-30 00:23:03 +0530

    feat: Delete & duplicate node

--- app/workflow/_components/nodes/NodeHeader.tsx
+++ app/workflow/_components/nodes/NodeHeader.tsx
@@ -1,12 +1,24 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { CreateFlowNode } from "@/lib/workflow/CreateFlowNode";
 import { TaskRegistry } from "@/lib/workflow/task/taskRegistry";
+import { AppNode } from "@/types/appNode";
 import { TaskType } from "@/types/task";
-import { CoinsIcon, GripVerticalIcon } from "lucide-react";
+import { useReactFlow } from "@xyflow/react";
+import { CoinsIcon, CopyIcon, GripVerticalIcon, TrashIcon } from "lucide-react";
+import App from "next/app";
 import React from "react";
 
-function NodeHeader({ taskType }: { taskType: TaskType }) {
+function NodeHeader({
+  taskType,
+  nodeId,
+}: {
+  taskType: TaskType;
+  nodeId: string;
+}) {
   const task = TaskRegistry[taskType];
+  const { deleteElements, getNode, addNodes } = useReactFlow();
+
   return (
     <div className="flex items-center gap-2 p-2">
       <task.icon size={16} />
@@ -20,6 +32,32 @@ function NodeHeader({ taskType }: { taskType: TaskType }) {
             <CoinsIcon size={16} />
             TODOS
           </Badge>
+          {!task.isEntryPoint && (
+            <>
+              <Button
+                variant={"ghost"}
+                onClick={() => deleteElements({ nodes: [{ id: nodeId }] })}
+              >
+                <TrashIcon size={12} />
+              </Button>
+              <Button
+                variant={"ghost"}
+                onClick={() => {
+                  const node = getNode(nodeId) as AppNode;
+                  const nodeX = node.position.x;
+                  // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+                  const nodeY = node.position.y + node.measured?.height! + 20;
+                  const newNode = CreateFlowNode(node.data.type, {
+                    x: nodeX,
+                    y: nodeY,
+                  });
+                  addNodes([newNode]);
+                }}
+              >
+                <CopyIcon size={12} />
+              </Button>
+            </>
+          )}
           <Button
             variant={"ghost"}
             size={"icon"}
